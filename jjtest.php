70d52f6c90681a50c595d2c803007a3f9006eda4
Jebin Joseph <jebin.joseph@outlook.com>
2024-11-12 21:09:14 -0500

    my first commit

---
+++ jjtest.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn PHP</title>
+</head>
+<body>
+    <?php
+
+    echo("<h1>Jesus Christ is Lord </h1>");
+
+    ?>
+</body>
+</html>
