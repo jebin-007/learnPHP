cb071be3985ab79788775fa7a4f2f5706b43f200
Jebin Joseph <jebin.joseph@outlook.com>
2024-11-12 21:36:22 -0500

    fourth commit form T490

--- jjtest.php
+++ jjtest.php
@@ -9,8 +9,9 @@
     <?php
 
     echo("<h1>Jesus Christ is Lord </h1>");
-    echo "This is one test";
-    echo "There is none like you";
+    echo "<h2>This is none like you</h2>";
+    echo "<p>There is none like you</p>";
+    echo "<p>None else can touch my heart like you do</p> ";
 
     ?>
 </body>
